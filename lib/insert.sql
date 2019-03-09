f74917fb686ea3c817a7d0fad1c39713832fad72
J. Singleton <jennifer.singleton@gmail.com>
2019-03-09 22:03:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,10 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "Male", "brown", "saucy", 1)
+
+-- Mr. Chocolate
+-- * Rowdy
+-- * Tabitha
+-- * Sergeant Brown
+-- * Melissa
+-- * Grinch
+-- * Wendy
+-- * unnamed
